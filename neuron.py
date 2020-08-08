c9ce7ee6c5c6565d227a68a089946b89f5c00f24
paul wright <cookyjnr@gmail.com>
2020-08-08 16:39:29 +1000

    created Dense_Laver class

--- neuron.py
+++ neuron.py
@@ -1,24 +1,30 @@
 import numpy as np
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
+
+X = [[1.0, 2.0, 3.0, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+
   
-weights = [[0.2, 0.8, -0.5, 1.0], 
-            [0.5, -0.91, 0.26, -0.5], 
-            [-0.26, -0.27, 0.17, 0.87]]
+class Layer_Dense:
+    # inputs: num inputs from previous layer
+    # neurons: number of neurons in current layer
+    def __init__(self, n_inputs, n_neurons):
+        #initialise weights for each input in batch
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        #initialise a vector of biases for each neron at 0
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, batch):
+        #dot product of input and weight for each input in batch
+        self.output = np.dot(batch, self.weights) + self.biases
 
-biases = [ 2, 3, 0.5 ]
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
-weights2 = [[0.1, -0.14, 0.5], 
-            [-0.5, 0.12, -0.33], 
-            [-0.44, 0.73, -0.13]]
-
-biases2 = [ -1, 2, -0.5 ]
+layer1.forward(X)
+layer2.forward(layer1.output)
 
 
-layer1_output = np.dot(inputs, np.array(weights).T) + biases
-
-layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
-
-print(layer2_output)
+print(layer2.output)
